531209ffdb9f328a8171d210b42ba54bda952a19
Piotr Smialkowski <p.smialkowski@gmail.com>
2023-08-24 23:43:50 +0200

    Add keys for nvim

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,15 +1,41 @@
+local set = vim.keymap.set
+
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+set("n", "<leader>ex", vim.cmd.Ex)
 
--- useful tab mapping
-vim.keymap.set("n", "<leader>te", vim.cmd.tabnew)
-vim.keymap.set("n", "<leader>tn", vim.cmd.tabnext)
-vim.keymap.set("n", "<leader>tp", vim.cmd.tabprev)
-vim.keymap.set("n", "<leader>to", vim.cmd.tabonly)
-vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose)
-vim.keymap.set("n", "<leader>tm", vim.cmd.tabmove)
+-- tab mapping
+set("n", "<leader>te", vim.cmd.tabnew)
+set("n", "<leader>tn", vim.cmd.tabnext)
+set("n", "<leader>tp", vim.cmd.tabprev)
+set("n", "<leader>to", vim.cmd.tabonly)
+set("n", "<leader>tc", vim.cmd.tabclose)
+set("n", "<leader>tm", vim.cmd.tabmove)
 
-vim.keymap.set({ "i", "v" }, "jk", "<ESC>", { remap = false, nowait = true })
-vim.keymap.set("n", "<leader>l", ":set relativenumber!<CR>")
-vim.keymap.set("n", "<leader>w", vim.cmd.write)
+set("n", "<leader>w", vim.cmd.write)
+set("n", "<leader>l", ":set relativenumber!<CR>")
 
+set({"n", "i", "v", "c"}, "<C-c>", "<Esc>")
+set({ "i", "v" }, "jk", "<ESC>", { remap = false, nowait = true })
+
+-- Split
+set("n", "<leader>v", ":vsp<CR>")
+set("n", "<leader>h", ":sp<CR>")
+
+-- window resize
+set("n", "<S-up>", ":resize -5<CR>")
+set("n", "<S-down>", ":resize +5<CR>")
+
+-- jump
+set("n", "J", "5j")
+set("n", "K", "5k")
+
+-- move lines
+set("n", "<A-j>", ":m .+1<CR>")
+set("n", "<A-k>", ":m .-2<CR>")
+set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
+-- ctrl+q to quit
+set("n", "<C-q>", ":q<CR>")
