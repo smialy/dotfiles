41b8e04d2fa458d48f44eaef4ba55f5c9ced7001
Piotr Smialkowski <p.smialkowski@gmail.com>
2023-08-24 20:51:48 +0200

    :lipstick: Clean

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,6 @@ return {
             ensure_installed = {
                 "lua",
                 "query",
-                "bash",
                 "python",
                 "javascript",
                 "typescript",
