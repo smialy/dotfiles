e9617b27b1bff5f782e86d8cb2d656e59cb566bf
Piotr Smialkowski <p.smialkowski@gmail.com>
2023-09-15 00:45:51 +0200

    Map up/down keys to term

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,12 +2,20 @@ return {
     {
         'akinsho/toggleterm.nvim', 
         version = "*", 
-        config = function(_, opts)
+        config = function()
             require("toggleterm").setup({
                 open_mapping = [[<c-\>]],
                 direction = "float",
                 shade_terminals = false,
             })
+            local opts = {}
+            vim.keymap.set('t', '<A-k>', '<Up>', opts)
+            vim.keymap.set('t', '<A-j>', '<Down>', opts)
+            vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+            vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+            vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+            vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+            vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
         end
     }
 }
